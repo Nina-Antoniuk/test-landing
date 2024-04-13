f777c5a64818a0d73a5f31e1c6a3755bc9093494
Nina-Antoniuk <78640356+Nina-Antoniuk@users.noreply.github.com>
2024-04-13 12:43:09 +0300

    dist

---
+++ dist/de3a8609c95b4e6e9022.js
@@ -0,0 +1,23 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './style.css';
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  }
+});
+const url = new URL(window.location.href);
+if (url.searchParams.has('locale')) {} else if (!url.searchParams.has('locale')) {
+  url.searchParams.set('locale', 'ua');
+  history.pushState({}, '', url);
+}
