ce0b14692dc4ffde72704cc49e2248498a38daf0
Nina-Antoniuk <78640356+Nina-Antoniuk@users.noreply.github.com>
2024-04-13 12:43:09 +0300

    dist

---
+++ 6ab9ca787a0661fc61e5.js
@@ -0,0 +1,31 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './style.css';
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  }
+});
+const url = new URL(window.location.href);
+if (url.searchParams.has('locale')) {} else if (!url.searchParams.has('locale')) {
+  url.searchParams.set('locale', 'ua');
+  history.pushState({}, '', url);
+}
+const burgerMenuBtn = document.querySelector('#mobile-menu-btn');
+const mobileNav = document.querySelector('#mobile-nav');
+const toggleMobileMenu = e => {
+  console.log('e', e);
+  mobileNav.classList.toggle('open');
+  console.log(mobileNav.classList.add('open'));
+};
+burgerMenuBtn.addEventListener('click', toggleMobileMenu);
---
+++ 838877ed0c80db32c766.js
@@ -0,0 +1,23 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './style.css';
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  }
+});
+const PATH_NAME = '/dist/index.html';
+console.log(window.location.pathname);
+if (window.location.pathname === PATH_NAME) {
+  window.location.href = `${PATH_NAME}/ua`;
+}
